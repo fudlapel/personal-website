2b2c23542fde783bc66077f46828351d6fbb27d0
Siri McClean <nmcclean@wellesley.edu>
2019-01-07 21:19:03 -0500

    added activity icons

--- src/components/activity-banner.js
+++ src/components/activity-banner.js
@@ -1,20 +1,34 @@
 import React from 'react'
 import styles from './activity-banner.module.css'
+const YogaIcon = require('../images/activity/yoga.png')
+const VBIcon = require('../images/activity/vb.png')
+const ReadingIcon = require('../images/activity/reading.png')
 
 const ActivityBanner = () => (
   <div className={styles.banner}>
     <div className={styles.innerbanner}>
       <p className={styles.activities}>
-        When I'm not coding, you can find me...
+        When I'm not coding you can find me doing yoga, playing volleyball or
+        reading!
       </p>
 
       <ul className={styles.activities}>
-        <li>YOGA</li>
-        <li>---</li>
-        <li>READING</li>
-        <li>---</li>
-        <li>TRAVEL/VB/POTTERY/</li>
+        <li>
+          {' '}
+          <img src={YogaIcon} alt="yoga icon" className={styles.icons} />
+        </li>
+        <li>
+          {' '}
+          <img src={VBIcon} alt="volleyball icon" className={styles.icons} />
+        </li>
+        <li>
+          {' '}
+          <img src={ReadingIcon} alt="reading icon" className={styles.icons} />
+        </li>
       </ul>
+      {/* <p className={styles.activities}>
+        doing yoga, playing volleyball or reading!
+      </p> */}
     </div>
   </div>
 )
