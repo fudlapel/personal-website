0ad5f3da693c91dd821097b715b74939c24b8009
Siri McClean <nmcclean@wellesley.edu>
2019-01-24 11:56:05 -0500

    added noreiga theme

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -3,4 +3,7 @@ import Typography from 'typography'
 import noriegaTheme from 'typography-theme-noriega'
 const typography = new Typography(noriegaTheme)
 
+// import fairyGatesTheme from 'typography-theme-fairy-gates'
+// const typography = new Typography(fairyGatesTheme)
+
 export default typography
