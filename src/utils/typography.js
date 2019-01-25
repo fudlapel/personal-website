03b7780f875c4bf12690a4334f7f8e6a98ccd729
Siri McClean <nmcclean@wellesley.edu>
2019-01-24 21:28:45 -0500

    changes to intro panel css

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -3,7 +3,4 @@ import Typography from 'typography'
 import noriegaTheme from 'typography-theme-noriega'
 const typography = new Typography(noriegaTheme)
 
-// import fairyGatesTheme from 'typography-theme-fairy-gates'
-// const typography = new Typography(fairyGatesTheme)
-
 export default typography
