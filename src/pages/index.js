f6ddf3bf2cb63a8f9f6a963d93d94e3a34e9d5b0
Siri McClean <nmcclean@wellesley.edu>
2019-01-29 15:54:10 -0500

    index clean up

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,5 @@
 import React from 'react'
-// import { Link } from 'gatsby'
-
 import Header from '../components/header'
-// import Layout from '../components/layout'
 import IntroPanel from '../components/intro-panel'
 import ProjectPanel from '../components/project-panel'
 import ActivityBanner from '../components/activity-banner'
@@ -13,10 +10,7 @@ import ContactBanner from '../components/contact-banner'
 const IndexPage = () => (
   <Container>
     <Header />
-    {/* <Layout> */}
     <IntroPanel />
-    {/* <Link to="/page-2/">Go to page 2</Link> */}
-    {/* </Layout> */}
     <ActivityBanner />
     <ProjectPanel />
     {/* <ExperiencePanel /> */}
